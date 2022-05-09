449a33fb9427f12641bf7b36e91e58b866705b66
miia12 <805417874@qq.com>
2022-05-08 19:38:29 -0500

    combine 5 sentences

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from sentence_generator import sentence_generator
 
 
 
-app = FastAPI()
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
+# app = FastAPI()
+# origins = ["*"]
+# app.add_middleware(
+#     CORSMiddleware,
 
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
 
-)
+# )
 
 story_gen = pipeline("text-generation", "jcpwfloi/gpt2-story-generation", handle_long_generation="hole", early_stopping=False)
 
@@ -29,14 +29,18 @@ def generate(sentence):
         LOGGER.error(e)
         return {'status': False, 'msg': e}, 400
 
-@app.post('/input')
-async def input(words: list = None):
+def input(words: list = None):
     try:
         results = sentence_generator(words)
-        return generate(results[0])
+        generate_text = ''
+        for sentence in results:
+            raw_text = generate(sentence)[0]['generated_text']
+            
+        return generate_text
     except Exception as e:
         LOGGER.error(e)
         return {'status': False, 'msg': e}, 400
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='0.0.0.0', port=8000)
+    # uvicorn.run(app=app, host='0.0.0.0', port=8000)
+    print(input(["jack", "eat breakfast", "college", "northwestern", "watch tv", "pizza", "play on the phone"]))
