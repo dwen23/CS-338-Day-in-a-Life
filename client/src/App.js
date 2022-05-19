41db0c00ae26ab20c467041a5be8c1d371e00436
isaiaslipa <isaiaslipa2023@u.northwestern.edu>
2022-05-19 11:23:53 -0500

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,189 @@
 import './App.css';
 import React from 'react';
+import { Home } from './components/home'
+import { Mornings } from './components/Mornings'
+import { Living } from './components/Living'
+import { Work } from './components/Work'
+import { DoWork } from './components/DoWork'
+import { Nothing } from './components/Nothing'
+import { WhyNothing } from './components/WhyNothing'
+import { School } from './components/School'
+import { Major } from './components/Major'
+import { Food } from './components/Food'
+import { Fear } from './components/Fear';
+import { LoadingIcon } from './components/LoadingIcon'
 
+
+let jsonData = [];
+let tempData = ["Emily", "take a shower", "college", "Northwestern", "hang out with my friends", "pasta", "finish my homework"];
 function App() {
-  const [valueA, setValueA] = React.useState(0);
+  const [currQuestion, setcurrQuestions] = React.useState('name');
+  const [ValueA, setValueA] = React.useState("");
 
-  var jsonData = ["Emily", "take a shower", "college", "Northwestern", "hang out with my friends", "pasta", "finish my homework"]
 
-  function handleClick() {
+  function questions(index) {
+    switch (index) {
+      case "name":
+        return (
+          <Home />
+        )
+      case "mornings":
+        return (
+          <Mornings />
+        )
+      case "living":
+        return (
+          <Living />
+        )
+      case "work":
+        return (
+          <Work />
+        )
+      case "doWork":
+        return (
+          <DoWork />
+        )
+      case "nothing":
+        return (
+          <Nothing />
+        )
+      case "whyNothing":
+        return (
+          <WhyNothing />
+        )
 
+      case "school":
+        return (
+          <School />
+        )
+      case "major":
+        return (
+          <Major />
+        )
+      case "food":
+        return (
+          <Food />
+        )
+      case "fear":
+        return (
+          <Fear />
+        )
+      case "done":
+        return (
+          <>
+            {ValueA === '' ? (
+              <LoadingIcon />
+            ) : (
+              <div>
+
+                <h5>
+                  {ValueA}
+                  <button class="btn btn-info" onClick={refreshPage()}>
+                    Try again
+                  </button>
+                </h5>
+
+              </div>
+            )}
+          </>
+        )
+    }
+  }
+
+  function refreshPage() {
+    window.location.reload();
+  }
+
+  function toNextQuestion() {
+    var e = document.getElementById(currQuestion);
+    var strUser = e.getAttribute('nextQuestion');
+    jsonData.push(e.value)
+    console.log(jsonData)
+    if (strUser === 'options') {
+      setcurrQuestions(e.value)
+    } else {
+      setcurrQuestions(strUser)
+    }
+  }
+
+  function currButton() {
+    if (currQuestion === 'fear') {
+      return (
+        <button type="button" class="btn btn-success" onClick={summitAnswers}>
+          Summit
+        </button>
+      )
+    } else if (currQuestion === 'done') {
+      return (
+        <div></div>
+      )
+    }
+    return (
+      <button type="button" class="btn btn-primary" onClick={toNextQuestion}>
+        Next
+      </button>
+    )
+  }
+
+
+  function summitAnswers() {
+    setcurrQuestions('done')
     // Send data to the backend via POST
-    fetch('http://0.0.0.0:8000/generate', {  // Enter your IP address here
+    fetch('http://127.0.0.1:8000/generate', {  // Enter your IP address here
 
       method: 'POST',
       mode: 'cors',
       headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
-      body: JSON.stringify(jsonData) // body data type must match "Content-Type" header
+      body: JSON.stringify(tempData) // body data type must match "Content-Type" header
     }).then(response => response.json()).then(data => { console.log(data); setValueA(data.text) });
   }
 
   return (
     <div>
-      <div onClick={handleClick} style={{
-        textAlign: 'center',
-        width: '100px',
-        border: '1px solid gray',
-        borderRadius: '5px'
-      }}>
-        Send data to backend
-      </div>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Day in the Life</span>
+        </div>
+      </nav>
+
       <div>
-        {valueA}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+
+      <div class="container">
+        <div class='row'>
+          <div class="col">
+          </div>
+          <div class="col-7">
+            {questions(currQuestion)}
+          </div>
+          <div class="col">
+          </div>
+        </div>
+
+        <div>
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+
+        <div class="row">
+          <div class="col">
+          </div>
+          <div class="col-5">
+
+          </div>
+          <div class="col">
+            {currButton()}
+          </div>
+        </div>
       </div>
     </div>
   );
